d56e1b51095b4fce57023f6b7303e4d20da0aa9f
Alvin Kamau <alvin.kamau@student.moringaschool.com>
2024-10-17 02:43:07 +0300

    commit 2

---
+++ src/components/About.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+const AboutSection = () => {
+    return (
+      <div className="about-section">
+        <div className="container">
+          <h2 className="about-section-title text-center">About Us</h2>
+          <div className="about-content-wrapper">
+            <div className="about-content-box">
+              <div className="about-icon-wrapper">
+                <img src="https://cdn3.iconfinder.com/data/icons/aami-web-internet/64/aami10-31-128.png" alt="Mission Icon" className="img-fluid" />
+              </div>
+              <h3 className="about-content-title">Our Mission</h3>
+              <p className="about-content-text">
+                We are dedicated to providing the best services to our customers. Our mission is to 
+                deliver high-quality products and exceptional customer service that exceeds your expectations.
+              </p>
+            </div>
+            <div className="about-content-box">
+              <div className="about-icon-wrapper">
+                <img src="https://cdn0.iconfinder.com/data/icons/untact-economy-1/32/ecommerce_career_rock_climber_climber_mission_career_objective-128.png" alt="Vision Icon" className="img-fluid" />
+              </div>
+              <h3 className="about-content-title">Our Vision</h3>
+              <p className="about-content-text">
+                To be a leader in our industry, known for our commitment to innovation and sustainability. 
+                We strive to create a positive impact in the communities we serve.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+
+export default AboutSection
---
+++ src/components/NavBar.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const NavBar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg" style={{ backgroundColor: '#ffffff', boxShadow: '0 2px 5px rgba(0, 0, 0, 0.1)' }}>
+      <div className="container-fluid" style={{ padding: "20px" }}>
+        <a className="navbar-brand" href="#" style={{ color: '#333333' }}>
+          <i className="bi bi-house"></i> Realator
+        </a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div className="navbar-nav ms-auto">
+            <Link className="nav-link" to="/" style={{ color: '#333333' }}>
+              <i className="bi bi-house-door" style={{ marginRight: '5px' }}></i> Home
+            </Link>
+            <Link className="nav-link" to="/features" style={{ color: '#333333' }}>
+              <i className="bi bi-star" style={{ marginRight: '5px' }}></i> Features
+            </Link>
+            <Link className="nav-link" to="/pricing" style={{ color: '#333333' }}>
+              <i className="bi bi-cash" style={{ marginRight: '5px' }}></i> Pricing
+            </Link>
+            <Link className="nav-link disabled" aria-disabled="true" to="#" style={{ color: '#cccccc' }}>
+              <i className="bi bi-lock" style={{ marginRight: '5px' }}></i> Disabled
+            </Link>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
